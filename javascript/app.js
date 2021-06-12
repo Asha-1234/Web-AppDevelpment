1e849a7767923509e715ca34e1de23b4efba57c6
Asha <asharathore@yahoo.com>
2021-06-12 16:39:52 +0500

    Calculator and Events in JS

---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+// function getNumber(num){
+//     var result = document.getElementById("result")
+//     result.value += num
+// }
+// function clearResult(){
+//     var result = document.getElementById("result")
+//     result.value = ""
+// }
+// function getResult(){
+//     var result = document.getElementById("result")
+//     result.value = eval(result.value)
+// }
+
+// // function readMore(){
+// //     var text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur vero illo aspernatur, accusamus quo incidunt, voluptates dolor sapiente fugit voluptate consequatur id saepe odit voluptatibus dicta sunt sint quaerat optio."
+// //     var para = document.getElementById("para")
+// //     para.innerHTML = text
+// // }
+// function para(){
+//     var t = document.getElementById("paragraph").innerHTML
+//     var p = document.getElementById("para")
+//     p.innerHTML = t 
+// }
+
+// function big(){
+//     var img = document.getElementById("flower")
+//     // img.className = 'hidden'
+//     img.className += ' hidden'
+// }
+
+// function changeImg(){
+//     var img = document.getElementById("img")
+//     img.src= "f2.jpg"
+// }
+
+// function firstImage(){
+//     var img = document.getElementById("img")
+//     img.src = "flower.jpg"
+// }
+
+// function changeImg(id, img){
+//     var image = document.getElementById(id)
+//     image.src = img
+// }
+
+// function changeImg(){
+//     var image = document.getElementById('img');
+//     image.style.width = "700px"
+
+//     var image = document.getElementById('para');
+//     image.style.color = "brown"
+// }
+
+
+        var para = document.getElementsByTagName('p');
+        para[0].style.color = 'red'
+        para[2].style.color = 'red'
